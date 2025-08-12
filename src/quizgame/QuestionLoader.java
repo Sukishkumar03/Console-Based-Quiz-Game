bf393f91dff090b1c1d9b9ac9d2fcbb1d1d54f3a
Sukishkumar03 <211801853+Sukishkumar03@users.noreply.github.com>
2025-08-12 18:34:52 +0530

    changed question format and added all question in a ArrayList.

--- src/quizgame/QuestionLoader.java
+++ src/quizgame/QuestionLoader.java
@@ -1,4 +1,26 @@
 package quizgame;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
 public class QuestionLoader {
+    static ArrayList<String> questions = new ArrayList<>();
+
+    public static void readQuestion() throws IOException {
+        String path = "/home/sukish/IdeaProjects/ConsoleBasedQuizGame/questions";
+        FileReader fr = new FileReader(path);
+        BufferedReader br = new BufferedReader(fr);
+        while(true){
+            String line = br.readLine();
+            if(line == null){
+                break;
+            }
+            questions.add(line);
+        }
+        System.out.println(questions);
+    }
 }
--- src/quizgame/QuizGame.java
+++ src/quizgame/QuizGame.java
@@ -1,7 +1,13 @@
 package quizgame;
 
+import java.io.IOException;
+
 public class QuizGame {
     public static void main(String[] args) {
-        
+        try {
+            QuestionLoader.readQuestion();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
