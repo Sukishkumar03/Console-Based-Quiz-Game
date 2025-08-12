f0ee1ef1db803cc356af74520a4b0b2adf541a11
Sukishkumar03 <211801853+Sukishkumar03@users.noreply.github.com>
2025-08-12 11:39:45 +0530

    Added required fields and constructor and getter in Question.java and correct answer options in the question.txt file

--- src/quizgame/Question.java
+++ src/quizgame/Question.java
@@ -1,4 +1,16 @@
 package quizgame;
 
 public class Question {
+    String questionText;
+    String[] options;
+    private final char correctOption;
+    public Question(String questionText, String[] options, char correctOption){
+        this.questionText = questionText;
+        this.options = options;
+        this.correctOption = correctOption;
+    }
+
+    public char getCorrectOption() {
+        return correctOption;
+    }
 }
